0260d9b0d0ce376a1b1df6f6fdfae1fe18529fb2
viviannemelo <viviannemmelo@gmail.com>
2023-07-11 16:24:23 -0300

    task 1 | students counter

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    for start, end in permanence_period:
+        try:
+            if start <= int(target_time) <= end:
+                counter += 1
+        except TypeError:
+            return None
+
+    return counter
