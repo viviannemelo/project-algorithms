7bab2be55ebe2992ac7e87018bafa696c329e674
viviannemelo <viviannemmelo@gmail.com>
2023-07-12 18:30:01 -0300

    task 2 | test encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,16 +3,14 @@ import pytest
 
 
 def test_encrypt_message():
-    with pytest.raises(TypeError):
-        encrypt_message("message", "key")
+    # with pytest.raises(TypeError):
+    #     encrypt_message("message", "key")
 
     with pytest.raises(TypeError):
-        encrypt_message("Teste", 3)
+        encrypt_message(123, "1")
 
-    assert encrypt_message("Teste", 0) == "etseT"
+    assert encrypt_message("Teste", -5) == "etseT"
 
-    assert encrypt_message("Algoritmos", 2) == "somtirog_lA"
+    assert encrypt_message("Algoritmos", 3) == "glA_somtiro"
 
     assert encrypt_message("vivianne", 4) == "enna_iviv"
-
-    assert encrypt_message("", 3) == ""
