bdbe24a96d35c0bbc618c1a2e5c1d87529deb3dd
viviannemelo <viviannemmelo@gmail.com>
2023-07-12 17:21:50 -0300

    task 2 | incomplete?

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+
+    with pytest.raises(TypeError):
+        encrypt_message("Teste", 3)
+
+    assert encrypt_message("Teste", 0) == "etseT"
+
+    assert encrypt_message("Algoritmos", 2) == "somtirog_lA"
+
+    assert encrypt_message("vivianne", 4) == "enna_iviv"
+
+    assert encrypt_message("", 3) == ""
